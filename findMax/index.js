174218d8908f0cec29b961f08e3ee11dc39745b2
ubt.zhang <zigzag.zhang@ubtrobot.com>
2019-04-11 20:52:23 +0800

    get max repeat numbers

--- findMax/index.js
+++ findMax/index.js
@@ -1,6 +1,17 @@
 const findMax = (arr) => {
+    let newObj = {}
     if(!arr.length) return '数组不能为空噢~'
     if (arr.length == 1) return arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        const e = arr[i];
+        if(!newObj[e]) newObj[e] = 0
+        newObj[e] = newObj[e] + 1
+    }
+    let objKeys = Object.keys(newObj)
+    let objVaule = Object.values(newObj)
+    let maxIndex = objVaule.indexOf(Math.max(...objVaule))
+    let maxRepeatValue = objKeys[maxIndex]
+    return objVaule
 }
 
 export default findMax
