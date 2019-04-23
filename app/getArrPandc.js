785bd37c0a9d0858c573957c888008c2e5ecd342
ubt.zhang <zigzag.zhang@ubtrobot.com>
2019-04-23 10:05:40 +0800

    add getArrPa

--- app/getArrPandc.js
+++ app/getArrPandc.js
@@ -7,11 +7,15 @@
 */
 
 const getArr = ( size ) => {
-   for (let i = 0; i < size; i++) {
-       const e = array[i];
-       
-   }
+    let resArr = []
+    for (let i = 0; i < size ** 2; i++) {
+        let e = i.toString(2)
+        if(e.length < 4) e = Array(size - e.length + 1).join(0) + e 
+        let eArr = e.split('').map(item => Number(item))
+        resArr.push([eArr])
+    }
+    return resArr
 }
 
 let arr = getArr(4);        //    给定组合长度为4
-console.log( JSON.stringify(arr) );
+console.log( arr );
