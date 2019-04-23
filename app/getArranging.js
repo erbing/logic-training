cfd52efe10258481513e003d9d6085d4b147f434
ubt.zhang <zigzag.zhang@ubtrobot.com>
2019-04-23 16:20:14 +0800

    change

---
+++ app/getArranging.js
@@ -0,0 +1,23 @@
+
+/**
+ * @param  {} a
+ * @param  {} b
+ * @param  {} c
+ */
+
+var a = [0, 1];
+var b = [3, 4, 5];
+var c = [6, 7];
+
+
+const getArranging = (a, b, c) => {
+    //TODO - 返回参数的排列组合
+    let resArr = []
+    return resArr
+}
+
+
+
+var ret = getArranging( a, b, c );
+
+console.log(ret)
